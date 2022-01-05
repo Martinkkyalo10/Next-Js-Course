861e4bd2856d4758322446b60c371013d16444b3
martin kyalo <martinkkyalo10@gmail.com>
2022-01-05 18:45:17 +0300

    add functionality to navigate programmatically to the filted events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,25 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/eventList";
 import EventSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+
+  const router = useRouter();
+
+  // recieve props from event search component to set up programmatic navigation
+  function findEventHandler(year, month) {
+    // get full path
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
   return (
     <Fragment>
-      <EventSearch />
+      {/* pass findEventHandler to EventSearch Component */}
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </Fragment>
   );
