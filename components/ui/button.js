f7c54fa9ae9f582a8b247266f60e430e2e8fda9b
martin kyalo <martinkkyalo10@gmail.com>
2022-01-05 15:52:46 +0300

    add button component and icon and component styles using module.css.

---
+++ components/ui/button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import classes from "./button.module.css";
+
+export default function Button(props) {
+  // the link passes a text that is imported from outside components.
+  //   the props, children accaptes dynamic values
+  return (
+    //   the link components is used to prevent
+    // brownser default behavior of sending http request when clicked
+    <Link href={props.link}>
+      {/* the link tag is added to apply custom css style */}
+      <a className={classes.btn}>{props.children}</a>
+    </Link>
+  );
+}
