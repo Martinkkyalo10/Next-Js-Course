2d482266aee936d0d77c4040700cc7e1c9a840d2
martin kyalo <martinkkyalo10@gmail.com>
2022-01-05 19:31:23 +0300

    extracting data on catch all route or page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,49 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "./../../dummy-data";
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  // extract data leading to this page from the url using router
+
+  const filterData = router.query.slug;
+  // console.log(filterData);
+  // validate filter data
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  // if there is filtered data
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // transform year and month from string to numbers
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  // anticipate for when users entered url wrong formatt /events/abc/34/34
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  // if there are filtered events
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  // check if filtered events are faulty or an empty array
+  if (!filteredEvents || filteredEvents.lenght === 0) {
+    return <p>No events founds for the selected filter!</p>;
+  }
+
+  // outputting events
   return (
     <div>
       <h1>Filtered Events HomePage</h1>
