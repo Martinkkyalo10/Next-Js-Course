73a4c79e6de1f38a9a1bcf799f2f3f49437a8366
martin kyalo <martinkkyalo10@gmail.com>
2022-01-05 13:37:22 +0300

    setting up main pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventDetailsPage() {
+  return (
+    <div>
+      <h1>Event Details Page</h1>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function AllEventsPage() {
+  return (
+    <div>
+      <h1>This is all events page</h1>
+    </div>
+  );
+}
