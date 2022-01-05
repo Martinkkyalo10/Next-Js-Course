62ee5724acdf5c7c0849f2a0a7476a50dffa9024
martin kyalo <martinkkyalo10@gmail.com>
2022-01-05 17:41:23 +0300

    all events rendered to the events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,11 @@
 import EventList from "../../components/events/eventList";
 import { getAllevents } from "../../dummy-data";
 
-
 export default function AllEventsPage() {
   const events = getAllevents();
   return (
     <div>
-      <EventList items={getFeaturedEvents} />
+      <EventList items={events} />
     </div>
   );
 }
