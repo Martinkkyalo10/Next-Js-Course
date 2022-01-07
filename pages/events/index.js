84a8304e3978162f1378b5994212fcdc9a82d80d
martin kyalo <martinkkyalo10@gmail.com>
2022-01-07 17:01:27 +0300

    data fetching on the 'all events page' using getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,12 +2,12 @@ import { Fragment } from "react";
 import { useRouter } from "next/router";
 import EventList from "../../components/events/eventList";
 import EventSearch from "../../components/events/events-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 
-export default function AllEventsPage() {
-  const events = getAllEvents();
-
+export default function AllEventsPage(props) {
   const router = useRouter();
+  // destructure props object to get events
+  const { events } = props;
 
   // recieve props from event search component to set up programmatic navigation
   function findEventHandler(year, month) {
@@ -23,4 +23,17 @@ export default function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  // get all events
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    // revalidate the page to ensure all new events are picked up
+    revalidate: 60,
+  };
 }
