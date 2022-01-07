45aea46657ce93628f5bc3ddd9293f73d19c025c
martin kyalo <martinkkyalo10@gmail.com>
2022-01-07 19:48:55 +0300

    applying head component content across all components using root component: _app.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-export default function layout(props) {
+export default function Layout(props) {
   return (
     <Fragment>
       <main>{props.children}</main>
