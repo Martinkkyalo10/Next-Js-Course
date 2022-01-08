a6bc661176c7d97f125c800a5e1c297d73918d76
martin kyalo <martinkkyalo10@gmail.com>
2022-01-08 17:02:11 +0300

    getting data from the database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,11 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  // set up mongodb connection
+  const client = await MongoClient.connect(
+    "mongodb+srv://eventapp123:Drbrand2020@cluster0.a2igt.mongodb.net/eventapp?retryWrites=true&w=majority"
+  );
+
   if (req.method === "POST") {
     const { email, name, text } = req.body;
     // add server side validaton
@@ -16,20 +22,29 @@ export default function handler(req, res) {
 
     //   construct a new object for saving in the database
     const newComment = {
-      id: eventId,
       email,
       name,
       text,
+      eventId,
     };
-    console.log(newComment);
+    const db = client.db();
+
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
+
+    newComment.id = result.insertedId;
     res.status(201).json({ message: "Added comment.", comment: newComment });
   }
   if (req.method === "Get") {
-    const dummyList = [
-      { id: "a1", name: "Martin", text: "my first comment" },
-      { id: "a2", name: "Kyalo", text: "my second comment." },
-    ];
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    res.status(200).json({ comments: dummyList });
+    res.status(200).json({ comments: documents });
   }
+  client.close();
 }
